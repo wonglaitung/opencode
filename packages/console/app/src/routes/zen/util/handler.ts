5613c5f80cdea9ab2493ec1c8a1f1b9225f3aa4a
Frank <frank@anoma.ly>
2026-07-02 21:59:33 -0400

    zen: new inference

--- packages/console/app/src/routes/zen/util/handler.ts
+++ packages/console/app/src/routes/zen/util/handler.ts
@@ -268,7 +268,7 @@ export async function handler(
 
       // Try another provider => stop retrying if using fallback provider
       if (
-        !isNewInference &&
+        //!isNewInference &&
         res.status !== 200 &&
         // ie. 400 error is usually provider error like malformed request
         res.status !== 400 &&
